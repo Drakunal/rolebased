cc62b4a354087c7028242c4437fe5a3291354f7e
Drakunal <drakunaldey1@gmail.com>
2021-03-13 09:57:56 +0530

    add customer details small part done

--- add-customer-details.php
+++ add-customer-details.php
@@ -67,14 +67,14 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 			<main class="content">
 				<div class="container-fluid p-0">
 
-					<h1 class="h3 mb-3">Add Customer form</h1>
+					<h1 class="h3 mb-3">Add Customer Details form</h1>
 
 					<div class="row">
 						<div class="">
 							<div class="card">
 								<div class="card-header">
 									<!-- <h5 class="card-title">Add Employee form</h5> -->
-									<h6 class="card-subtitle text-muted">Add your customers here.</h6>
+									<h6 class="card-subtitle text-muted">Add your customer details here.</h6>
 								</div>
 								<div class="card-body">
 									<form enctype="multipart/form-data" method="post" action="">
@@ -83,18 +83,18 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 											<input type="text" name="email" class="form-control" placeholder="customer id">
 										</div>
 										<div class="mb-3">
-											<label class="form-label">Password</label>
-											<input type="password"  name="password"class="form-control" placeholder="Password">
+											<label class="form-label">Number of hours allocated</label>
+											<input type="time"  name="password"class="form-control" placeholder="number of hours">
 										</div>
                                         <div class="mb-3">
 											<label class="form-label">Name</label>
 											<input type="text" name="name" class="form-control" placeholder="Name">
 										</div>
                                         
-										<!-- <div class="mb-3">
-											<label class="form-label">Textarea</label>
-											<textarea class="form-control" placeholder="Textarea" rows="1"></textarea>
-										</div> -->
+										<div class="mb-3">
+											<label class="form-label">Details</label>
+											<textarea class="form-control" placeholder="Details" rows="1"></textarea>
+										</div>
 										<!-- <div class="mb-3">
 											<label class="form-label w-100">Upload Image</label>
 											<input type="file" name="image"> -->
@@ -275,6 +275,7 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 			<?php
 			if(isset($_POST['submit']))
 			{
+                    $user_email_id = $_GET['id'];
 					$email=$_POST['email'];
 					$password=$_POST['password'];
 					$name=$_POST['name'];
@@ -339,12 +340,12 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 					// }
 					// }
 
-					mysqli_query($db,"INSERT INTO `users` (user_id, password, role, name) VALUES('$email', '$password','$role', '$name');");
+					mysqli_query($db,"INSERT INTO `customer-details` (user_id, details, time_allocated) VALUES('$email', '$details','$time_allocated', '$name');");
 				
 					?>
 								<script>
 									
-									alert("Customer Saved");
+									alert("Customer Details Saved");
                                     window.location.href = "add-customer-details.php?id=".$row['user_id'];
 								
 									</script>
