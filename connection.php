c60fb493d5a79a9aa7f61543519ed3805f74b3b1
Drakunal <drakunaldey1@gmail.com>
2021-03-17 18:14:06 +0530

    connection modified

--- connection.php
+++ connection.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "10.27.18.48";
-$username = "swipemedia_serolebased@10.27.18.48";
-$password = "peteradmin123";
+$servername = "localhost";
+$username = "root";
+$password = "";
 
 // Create connection
-$db = new mysqli($servername, $username, $password,"swipemedia_serolebased");
+$db = new mysqli($servername, $username, $password,"rolebased");
 
 // Check connection
 if ($db->connect_error) {
   die("Connection failed: " . $db->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
