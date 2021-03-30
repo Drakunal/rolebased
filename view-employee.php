ad44a453e6377b78679d305cd00efa476aac13ae
Drakunal <drakunaldey1@gmail.com>
2021-03-30 11:48:07 +0530

    employee details added in view employee portion

--- view-employee.php
+++ view-employee.php
@@ -117,7 +117,7 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
                                                 </tr>
                                                 <?php
 												$e_id=$row['id'];
-												$sql1 = "SELECT base_price FROM employee_details WHERE user_id=`$e_id`";
+												$sql1 = "SELECT base_price,details FROM employee_details WHERE user_id=$e_id";
 												$result1 = mysqli_query($db,$sql1);
 
                                                 }
@@ -143,12 +143,16 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 											<h5 class="card-title mb-4">Employee Details</h5>
 											<?php 
 											if($result1){
-												echo "hi";
+												$row1=$result1->fetch_assoc(); ?>
+												<p><strong>Base Price : </strong><?php echo $row1['base_price']; ?> Kr</p>
+												<p><strong>Details : </strong><?php echo $row1['details']; ?></p>
+											<?php
+												
 
 											}
 											else{ ?>
 												<div class="mb-3">
-													<button class="btn btn-primary"><a style="text-decoration:none; color:white;" href="add-employee-details.php?id=<?php echo $e_id ?>">Add Customer Appointment</a></button>
+													<button class="btn btn-primary"><a style="text-decoration:none; color:white;" href="add-employee-details.php?id=<?php echo $e_id ?>">Add Employee Details</a></button>
 												</div>
 												
 											<?php } ?>
