eb5dbb860c4940a7e4293d50077bccb4c0999bda
Drakunal <drakunaldey1@gmail.com>
2021-04-02 19:54:57 +0530

    monthly updated to new ver in appointment add

--- add-appointment.php
+++ add-appointment.php
@@ -165,8 +165,27 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 				{
 					for ($x = 1; $x <= $appointment_duration; $x++) {
 						mysqli_query($db,"INSERT INTO `appointments` (customer_id, employee_id, date, time) VALUES('$customer_id','$employee_id', '$date','$time');");	
-					
-						$date=date('Y-m-d', strtotime($date. ' + 30 days'));
+						$date_temp=date('Y-m-d', strtotime($date));
+						$date_temp_month=date("F",strtotime($date_temp));  //month
+						// $date_temp_week=date("d",$date_temp); // day of the date
+						// $temp_week_number=ceil($date_temp_week/7); // week of the date
+
+
+						$date=date('Y-m-d', strtotime($date. ' + 28 days'));
+						$date_month=date("F",strtotime($date));
+						// $date_week=date("d",$date);
+						// $week_number=ceil($date_week/7);
+						if($date_temp_month==$date_month){ // it means the previous and current appointment are in same month so add +7
+							$date=date('Y-m-d', strtotime($date. ' + 7 days'));
+						}
+						// if($week_number!=$temp_week_number){
+
+						// 	$difference=abs($week_number-$temp_week_number);
+						// 	$difference_in_days=$difference*7;
+						// 	$date=date('Y-m-d', strtotime($date. ' - '.$difference_in_days.' days'));
+
+						// }
+
 					}
 					
 				}
---
+++ testing_date.php
@@ -0,0 +1,30 @@
+<?php
+$date=date("Y/m/d");
+for ($x = 1; $x <= 12; $x++) {
+    // mysqli_query($db,"INSERT INTO `appointments` (customer_id, employee_id, date, time) VALUES('$customer_id','$employee_id', '$date','$time');");	
+    $date_temp=date('Y-m-d', strtotime($date));
+    $date_temp_month=date("F",strtotime($date_temp)); //month
+    // $date_temp_week=date("d",$date_temp); // day of the date
+    // $temp_week_number=ceil($date_temp_week/7); // week of the date
+    // echo $date_temp_week;
+
+    $date=date('Y-m-d', strtotime($date. ' + 28 days'));
+    $date_month=date("F",strtotime($date));
+    // $date_week=date("d",$date);
+    // $week_number=ceil($date_week/7);
+    if( $date_temp_month== $date_month){
+        // echo "HI" ;// it means the previous and current appointment are in same month so add +7
+        $date=date('Y-m-d', strtotime($date. ' + 7 days'));
+    }
+echo "/n";
+    echo ($date);
+    // if($week_number!=$temp_week_number){
+
+    // 	$difference=abs($week_number-$temp_week_number);
+    // 	$difference_in_days=$difference*7;
+    // 	$date=date('Y-m-d', strtotime($date. ' - '.$difference_in_days.' days'));
+
+    // }
+
+}
+?>
