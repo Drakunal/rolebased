6575cb830ac77116af83d47b038b924e15e264c9
Drakunal <drakunaldey1@gmail.com>
2021-04-28 21:55:31 +0530

    settings page addeD

--- navbar.php
+++ navbar.php
@@ -168,10 +168,10 @@
 								<!-- <a class="dropdown-item" href="pages-profile.html"><i class="align-middle mr-1" data-feather="user"></i> Profile</a> -->
 								<!-- <a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="pie-chart"></i> Analytics</a> -->
 								<!-- <div class="dropdown-divider"></div> -->
-								<!-- <a class="dropdown-item" href="pages-settings.html"><i class="align-middle mr-1" data-feather="settings"></i> Settings & Privacy</a> -->
+								<a class="dropdown-item" href="settings.php"><i class="align-middle mr-1" data-feather="settings"></i> Settings & Privacy</a>
 								<!-- <a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="help-circle"></i> Help Center</a> -->
 								<!-- <div class="dropdown-divider"></div> -->
-								<a class="dropdown-item" href="logout.php">Log out</a>
+								<a class="dropdown-item" href="logout.php"><i class="align-middle mr-1" data-feather="log-out"></i>Log out</a>
 							</div>
 						</li>
 					</ul>
