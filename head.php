cd3626c9caa55cde2a0a0f80e2f7402ada4a9776
Drakunal <drakunaldey1@gmail.com>
2021-04-30 16:34:23 +0530

    head and scripts.php created

---
+++ head.php
@@ -0,0 +1,17 @@
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="AdminKit">
+	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+
+	<title>Monikas</title>
+    
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.bootstrap4.min.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
+</head>
--- test-list.php
+++ test-list.php
@@ -10,24 +10,7 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="AdminKit">
-	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-
-	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
-
-	<title>Tables | AdminKit Demo</title>
-    
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.bootstrap4.min.css" rel="stylesheet">
-
-    <link href="css/app.css" rel="stylesheet">
-</head>
+<?php include("head.php"); ?>
 
 <body>
 	<div class="wrapper">
@@ -566,26 +549,7 @@ if(!isset($_SESSION['login_user'])||$_SESSION['role']!="admin")
 		</div>
 	</div>
 
-    <script src="js/app.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-
-    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
-
-    <script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-
-    <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.bootstrap4.min.js"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script> -->
-
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script> -->
-    <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
-    <!-- <script scr="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script> -->
-    <!-- <script scr="https://cdn.datatables.net/buttons/1.7.0/js/buttons.colVis.min.js"></script> -->
-    <!-- <script scr="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script> -->
+    <?php include("scripts.php"); ?>
     <script>
 $(document).ready(function() {
     var table = $('#example').DataTable( {
